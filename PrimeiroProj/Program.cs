25144419ad7d74e3657e4197489cdbc01571392f
Lucas Ramos Silva <lucas.silva@7comm.com.br>
2020-01-10 15:51:24 -0300

    Introducing to dotnet

--- PrimeiroProj/Program.cs
+++ PrimeiroProj/Program.cs
@@ -7,9 +7,19 @@ namespace _primeiroprojeto
     {
         static void Main(string[] args)
         {
-            
+
+            Console.Write("\nDigite a quantidade de alunos: ");
+            int quant = int.Parse(Console.ReadLine());
+
+            while (quant <= 0)
+            {
+                Console.Write("Digite uma quantidade válida de alunos (maior que zero): ");
+                quant = int.Parse(Console.ReadLine());
+            }
+
             bool dec = true;
-            Aluno[] alunos = new Aluno[5];
+            Aluno[] alunos = new Aluno[quant];
+            int indice = 0;
             string opc;
 
             do
@@ -21,7 +31,7 @@ namespace _primeiroprojeto
                     case "1":
                         //Adicionar aluno
                         Console.Write("\nInforme o nome do aluno: ");
-                        Aluno aluno = new Aluno();
+                        var aluno = new Aluno();
                         aluno.nome = Console.ReadLine();
 
                         Console.Write("Informe a nota do aluno: ");
@@ -30,21 +40,27 @@ namespace _primeiroprojeto
                         {
                             if (dec)
                                 Console.Write("A nota do aluno deve ser decimal! Digite novamente: ");
-                            else
+                            else if ((nota < 0) || (nota > 10))
                                 Console.Write("A nota deve estar entre 0 e 10! Digite novamente: ");
+                            else
+                                aluno.nota = nota;
                         }
+                        alunos[indice] = aluno;
+                        indice++;
+
                         break;
                     case "2":
                         //Listar alunos
+                        for (int i = 0; i < indice; i++)
+                        {
+                            Console.WriteLine($"\n\t\tAluno {i+1} \nNome do aluno: {alunos[i].nome} \nNota do aluno: {alunos[i].nota}");
+                        }
                         break;
                     case "3":
                         //Calcular média geral
                         break;
                     case "0":
                         Console.WriteLine("\n\t\tAté a próxima!\n");
-                        break;
-                    default:
-                        opc = Validacao(opc);
                         break;
                 }
             } while (opc != "0");
@@ -56,6 +72,7 @@ namespace _primeiroprojeto
             string opc;
             Console.Write("\n\t\tMenu de opções: \n\n1- Inserir novo aluno \n2- Listar alunos \n3- Calcular média geral \n\n0- Sair \n\nOpção desejada: ");
             opc = Console.ReadLine();
+            opc = Validacao(opc);
             return opc;
         }
         
@@ -66,7 +83,9 @@ namespace _primeiroprojeto
             {
                 Console.Write("Opção inválida! Digite novamente: ");
                 opc = Console.ReadLine();
-                if (opc == "0")
+                if ((opc == "1") || (opc == "2") || (opc == "3"))
+                    continue;
+                else if (opc == "0")
                     Console.WriteLine("\n\t\tAté a próxima!\n");
             }
             return opc;
